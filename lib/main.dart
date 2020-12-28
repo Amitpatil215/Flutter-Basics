7481cd0a0bed7479ae35f8ecfdb29f762868bc43
Amit Patil <amitgpatil215@gmail.com>
2020-12-29 00:48:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+void main() {
+/* // int
+  int age;
+  age = 24;
+  print(age); */
+
+/* // double
+  double age = 26;
+  print(age);
+ */
+
+/* //num
+  num a = 12.4;
+  print(a); */
+
+/* // String
+  String name;
+  name = 'babita';
+  print(name); */
+
+/* // bool
+  bool isYes = true;
+  print(isYes);
+ */
+
+/* // List
+  List<int> ages = [24, 12, 32];
+  print(ages);
+ */
+/* // Map
+  Map<String, dynamic> login = {'user': 'babita@test.com', 'pass': 12345};
+  print(login);
+ */
+/* // Set
+  Set nation = {'India', 'Uk'};
+  print(nation);
+ */
+
+/* // var
+  var age = 'babita';
+  print(age);
+  print(age.runtimeType); */
+
+/* // dynamic
+  List a = []; */
+}
