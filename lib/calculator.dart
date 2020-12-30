628db28cc9461f467740f0c01a55228453d03d6d
Amit Patil <amitgpatil215@gmail.com>
2020-12-31 00:28:49 +0530

    arithmetic operator

---
+++ lib/calculator.dart
@@ -0,0 +1,21 @@
+//? Arithmetic Operators and functions
+//* Simple calculator
+//! (+, -, *, /, %)
+
+int add(int a, int b) {
+  //print(a + b);
+  return a + b;
+}
+
+void mod(int a, int b) {
+  print(a % b);
+}
+
+void main() {
+  int a = 12;
+  int b = 5;
+  // print(a + b);
+  // var sum = add(12, 5);
+  // print(sum);
+  mod(a, b);
+}
